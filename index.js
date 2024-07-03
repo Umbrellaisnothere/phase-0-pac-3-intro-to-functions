12a7c1423cc518c94fa444cc7c6bd4d7109340bf
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-03 15:14:22 +0300

    Intro-ffunctions-completed

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Follow along with the examples here
+function sayHelloToGuadalupe() {
+    console.log ("Hello!, Guadalupe!");
+}
+
+sayHelloToGuadalupe()
+
+function sayHelloToLiz() {
+    console.log ("Hello!, Liz!");
+}
+
+sayHelloToLiz()
+
+function sayHelloToSamip() {
+    console.log ("Hello!, Samip!");
+}
+
+sayHelloToSamip()
+
+//Separate
+
+function say(greeting ,firstName) {
+    console.log("firstname: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  
+  say( "Julio", "Goodbye");
+
+//Separate
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(80 ,9000));
+
+//Separate
+
+function say(greeting ,firstName) {
+    console.log("I was called!");
+    return(`${greeting}, ${firstName}!`);
+  }
+  
+  console.log(say("Howdy", "Ivy"));
